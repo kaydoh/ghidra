9b4d1d81c04a4bdb93c8c07ceae159da2f9bb72b
mumbel <22204578+mumbel@users.noreply.github.com>
2020-01-26 23:31:26 -0600

    Additions to pcodetest - adding tests for casting between types - added RISC-V pcode def - small build fix for f8 in f4 files - added missing command line args to default

--- Ghidra/Extensions/SleighDevTools/pcodetest/c_src/BIOPS_DOUBLE_BODY.c
+++ Ghidra/Extensions/SleighDevTools/pcodetest/c_src/BIOPS_DOUBLE_BODY.c
@@ -25,6 +25,19 @@ f8 f8_greaterThan(f8 lhs, f8 rhs)
 	return z;
 }
 
+f8 f8_compareLogic(f8 lhs, f8 rhs)
+{
+	if (lhs < 0)
+		lhs += 2;
+	if (lhs > 0)
+		lhs += 4;
+	if (lhs == 0)
+		lhs += 8;
+	if (lhs != rhs)
+		lhs += 16;
+	return lhs;
+}
+
 f8 f8_greaterThanEquals(f8 lhs, f8 rhs)
 {
 	f8 z;
--- Ghidra/Features/Base/src/main/java/ghidra/test/processors/support/ProcessorEmulatorTestAdapter.java
+++ Ghidra/Features/Base/src/main/java/ghidra/test/processors/support/ProcessorEmulatorTestAdapter.java
@@ -2221,5 +2221,20 @@ public abstract class ProcessorEmulatorTestAdapter extends TestCase implements E
 	public final void test_StructUnionManipulation() {
 		// stub
 	}
+	
+	public final void test_CONVERT() {
+		// stub
+	}
 
+	public final void test_CONVERT_DOUBLE() {
+		// stub
+	}
+	
+	public final void test_CONVERT_FLOAT() {
+		// stub
+	}
+	
+	public final void test_CONVERT_LONGLONG() {
+		// stub
+	}
 }
